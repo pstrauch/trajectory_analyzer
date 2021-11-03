fea77ec0469fb2850dd4704aa83f9a24c5f91496
Pascal <pstrauch@ethz.ch>
2021-11-03 17:47:31 +0100

    minor bug fixing

--- app/trajectory_analyzer.cpp
+++ app/trajectory_analyzer.cpp
@@ -17,23 +17,26 @@ int main()
     std::cout << "--Loading Data--" << std::endl;
 
     //extraction needed
-    bool extract_data = false;
-    //path to data
+    bool extract_data = false;  //PARAMETER
+    //path to data PARAMETER
     // std::string path = "./log/27_10_circle_delay_5ms.csv";
     // std::string path = "./log/26_10_circle_delay_1ms.csv";
     std::string path = "./log/test/testdata.csv";
 
     //define data vectors
+    int raw_data_columns = 2;   //PARAMETER
     std::vector<double> vec;    //helper
     //raw_data: time, x, y, z, roll, pitch, yaw
     // std::vector<std::vector<double>> raw_data{vec, vec, vec, vec, 
     //                                                 vec, vec, vec};
-    std::vector<std::vector<double>> raw_data{vec, vec};
+    std::vector<std::vector<double>> raw_data;
+    for(int i = 0; i < raw_data_columns; i++)
+        raw_data.push_back(vec);
     std::vector<std::vector<double>> data_points{vec, vec};
     std::vector<std::vector<double>> discretization{vec, vec};
 
     //read raw data
-    double factor = 1000;
+    double factor = 1;  //PARAMETER
     read_csv_to_col_vec(raw_data, path, factor);
     std::cout   << " *Data length: " 
                     << raw_data.at(1).size() 
@@ -45,10 +48,14 @@ int main()
 
         // extract_data(raw_data, data_points, 1, 2, 750, 1000);
         extract_data_from_col_vec(raw_data, data_points, 1, 2, 1600, 2000);
+        //PARAMETER
 
         std::cout   << " *Extracted data length: " 
                     << data_points.at(0).size() 
                     << std::endl;
+    }else
+    {
+        data_points = raw_data;
     }
 
     std::cout << "--Discretizing Trajectory--" << std::endl;
@@ -62,7 +69,7 @@ int main()
                         500,
                         -500); */
     discretize_line(discretization,
-                    2000,
+                    100,
                     0,
                     10,
                     0,
